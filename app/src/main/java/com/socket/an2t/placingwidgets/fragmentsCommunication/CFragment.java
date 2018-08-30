3f5bbbcd69fdb52043a9d38a3c3b5a094e9f360c
Anant S Awasthy <anant@binarynumbers.io>
2018-08-30 11:59:06 +0530

    temp initial

--- app/src/main/java/com/socket/an2t/placingwidgets/fragmentsCommunication/CFragment.java
+++ app/src/main/java/com/socket/an2t/placingwidgets/fragmentsCommunication/CFragment.java
@@ -105,5 +105,6 @@ public class CFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+        int i =10;
     }
 }
